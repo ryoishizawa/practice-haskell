7401c714c4c712b68e58cd393f9e158b10a56e37
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2021-07-19 05:06:55 +0800

    modify baby.hs

--- learn-you-a-haskell/chapter1/baby.hs
+++ learn-you-a-haskell/chapter1/baby.hs
@@ -6,3 +6,7 @@ doubleSmallNumber x = if x > 100
                         else x * 2
 
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+array = [1,2,3,4] ++ [5]
+-- the below does not work
+var array2 = [1,2,3,4] ++ [5]
